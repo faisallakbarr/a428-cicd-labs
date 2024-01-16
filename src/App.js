ed29a808529caf202d1e2eda3b7e8e4717478413
faisallakbarr <faisal.akbar0798@gmail.com>
2024-01-16 13:28:25 +0700

    merubah tampilan web react-app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React!</h1>
         </header>
+        <body>
+          <h1>Hallo Perkenalkan nama saya Faisal Akbar</h1>
+          <p>Saya sedang mencoba membuat web sederhana dengan reactjs dan menggunakan jenkins pipeline</p>
+        </body>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
