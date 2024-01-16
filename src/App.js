7d4d05f8596b43619fd76229c1d484d1af0c95f6
faisallakbarr <faisal.akbar0798@gmail.com>
2024-01-16 15:45:25 +0700

    Add fitur App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const [value, setValue] = useState('');
+    
 
     return (
       <div className="App">
@@ -15,44 +15,6 @@ class App extends Component {
           <p>Saya sedang mencoba membuat web sederhana dengan reactjs dan build jenkins pipeline</p>
         </header>
         <body className='App-body'>
-          <div className='container'>
-            <div className='calculator'>
-              <form action="">
-                <div className="display">
-                  <input type="text" value={value}/>
-                </div>
-                <div>
-                  <input type="button" value="AC" onClick={e => setValue('')}/>
-                  <input type="button" value="DEL" onClick={e => setValue(value.slice(0, -1))}/>
-                  <input type="button" value="." onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="/" onClick={e => setValue(value + e.target.value)}/>
-                </div>
-                <div>
-                  <input type="button" value="7" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="8" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="9" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="*" onClick={e => setValue(value + e.target.value)}/>
-                </div>
-                <div>
-                  <input type="button" value="4" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="5" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="6" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="+" onClick={e => setValue(value + e.target.value)}/>
-                </div>
-                <div>
-                  <input type="button" value="1" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="2" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="3" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="-" onClick={e => setValue(value + e.target.value)}/>
-                </div>
-                <div>
-                  <input type="button" value="00" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="0" onClick={e => setValue(value + e.target.value)}/>
-                  <input type="button" value="=" className='equal' onClick={e => setValue(eval(value))}/>
-                </div>
-              </form>
-            </div>
-          </div>
           
         </body>
         <footer className='App-footer'>
