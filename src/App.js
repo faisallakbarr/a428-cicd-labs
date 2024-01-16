3caed7238381f695ce74034afe148e33f4cd0b52
faisallakbarr <faisal.akbar0798@gmail.com>
2024-01-16 15:39:39 +0700

    Add fitur App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,27 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">Selamat datang di React App!</h1>
+          <h1 className='App-title'>Hallo Perkenalkan nama saya Faisal Akbar</h1>
+          <p>Saya sedang mencoba membuat web sederhana dengan reactjs dan build jenkins pipeline</p>
         </header>
-        <body>
-          <h1>Hallo Perkenalkan nama saya Faisal Akbar</h1>
-          <p>Saya sedang mencoba membuat web sederhana dengan reactjs dan menggunakan jenkins pipeline</p>
+        <body className='App-body'>
+          <div className='container'>
+            <div className='calculator'>
+              <form action="">
+                <div>
+                  <input type="text" />
+                </div>
+              </form>
+            </div>
+          </div>
+          
         </body>
+        <footer className='App-footer'>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </footer>
       </div>
     );
   }
